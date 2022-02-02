bd10ade2ee5029a8e038ce55d89606097ac89940
avestinvestment <thean781@gmail.com>
2022-02-02 16:52:35 -0500

    Init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
+
+const customMap = new CustomMap("map");
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
+
